a5bc853c6b39ac0158291d7458a99a830f0ff0e1
Carson Wilcox <cwilcox@akamai.com>
2015-01-29 00:47:16 -0500

    added check for correct usage of cli parameters

--- catchat-cli/main.cpp
+++ catchat-cli/main.cpp
@@ -1,7 +1,14 @@
 #include <catchat/catchat.hpp>
+#include <iostream>
+using std::cout;
 
-int main(int , char** )
+
+int main(int argc, char *argv[])
 {
+    if (argc != 0) {
+        cout<<"usage: "<<argv[0]<<"\n";
+        return 1;
+    }
     catchat::catchat init;
     catchat::node node;
     node.run_once();
